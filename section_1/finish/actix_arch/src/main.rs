420e3d0686e6f67b199ea07d1eced5297412713a
David Choi <dharric@live.com>
2023-06-04 20:22:02 -0400

    chore: add actix arch finish code

--- section_1/finish/actix_arch/src/main.rs
+++ section_1/finish/actix_arch/src/main.rs
@@ -1,6 +1,24 @@
+use actix_web::{HttpServer, web};
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
+    HttpServer::new(|| {
+        actix_web::App::new()
+            .route("/", web::get().to(index))
+            .service(
+                web::scope("/v1")
+                    .route("/item", web::get().to(get_item)) // append additional routes 
+            ) // append additional services
+    })
+        .bind(("127.0.0.1", 8001))?
+        .run()
+        .await
+}
 
-    Ok(())
+async fn index() -> &'static str {
+    "Hello World!"
+}
+
+async fn get_item() -> &'static str {
+    "Item A"
 }
