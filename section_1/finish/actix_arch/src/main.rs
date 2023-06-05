07d1a45952649f44387fa9bf411744f2756c5b8c
David Choi <dharric@live.com>
2023-06-04 21:34:26 -0400

    chore: complete actix arch section

--- section_1/finish/actix_arch/src/main.rs
+++ section_1/finish/actix_arch/src/main.rs
@@ -1,24 +1,24 @@
-use actix_web::{HttpServer, web};
+use actix_web::HttpServer;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         actix_web::App::new()
-            .route("/", web::get().to(index))
             .service(
-                web::scope("/v1")
-                    .route("/item", web::get().to(get_item)) // append additional routes 
-            ) // append additional services
+                actix_web::web::scope("/v1")
+                    .route("/profile", actix_web::web::get().to(index))
+                    .route("/profile", actix_web::web::post().to(insert))
+            )
     })
-        .bind(("127.0.0.1", 8001))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8001))?
+    .run()
+    .await
 }
 
 async fn index() -> &'static str {
-    "Hello World!"
+    "hello world"
 }
 
-async fn get_item() -> &'static str {
-    "Item A"
+async fn insert() -> &'static str {
+    "inserted"
 }
--- section_1/start/actix_arch/src/main.rs
+++ section_1/start/actix_arch/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::HttpServer;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        actix_web::App::new()
+            .service(
+                actix_web::web::scope("/v1")
+                    .route("/profile", actix_web::web::get().to(index))
+                    .route("/profile", actix_web::web::post().to(insert))
+            )
+    })
+    .bind(("127.0.0.1", 8001))?
+    .run()
+    .await
+}
+
+async fn index() -> &'static str {
+    "hello world"
+}
+
+async fn insert() -> &'static str {
+    "inserted"
 }
