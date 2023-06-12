70e6d156bb05c928a18a55c4e79e68c2e5cb2d7a
David Choi <dharric@live.com>
2023-06-12 13:38:36 -0400

    chore: added message router error unit tests

--- final/twitter-api/src/routes/messages/message_route.rs
+++ final/twitter-api/src/routes/messages/message_route.rs
@@ -1,15 +1,15 @@
 use crate::common::entities::messages::model::{MessageWithFollowingAndBroadcastQueryResult};
 use crate::common::app_state::AppState;
 use crate::common::entities::messages::repo::{InsertMessageFn, QueryMessageFn, QueryMessagesFn};
+use crate::routes::errors::error_utils::UserError;
 use crate::routes::output_id::OutputId;
 use crate::routes::profiles::model::ProfileShort;
 use actix_web::{web, web::{Path, Json}, Responder};
-use std::error::Error;
 use super::model::{MessageResponder, MessagePostJson, MessageQuery, MessageByFollowingQuery};
 
 
 #[allow(unused)]
-pub async fn create_message<T: InsertMessageFn>(app_data: web::Data<AppState<T>>, params: Json<MessagePostJson>) -> Result<OutputId, Box<dyn Error>> {  
+pub async fn create_message<T: InsertMessageFn>(app_data: web::Data<AppState<T>>, params: Json<MessagePostJson>) -> Result<OutputId, UserError> {  
     let max = 141; 
     let body = if params.body.len() < max {
         &params.body[..]
@@ -21,12 +21,12 @@ pub async fn create_message<T: InsertMessageFn>(app_data: web::Data<AppState<T>>
     let result = app_data.db_repo.insert_message(params.user_id, body, group_type, params.broadcasting_msg_id).await;
     match result {
         Ok(id) => Ok(OutputId { id }),
-        Err(e) => Err(Box::new(e))
+        Err(e) => Err(e.into())
     }
 }
 
 #[allow(unused)]
-pub async fn get_message<T: QueryMessageFn>(app_data: web::Data<AppState<T>>, path: Path<MessageQuery>) -> Result<Option<impl Responder>, Box<dyn Error>> {
+pub async fn get_message<T: QueryMessageFn>(app_data: web::Data<AppState<T>>, path: Path<MessageQuery>) -> Result<Option<impl Responder>, UserError> {
     let message_result = app_data.db_repo.query_message(path.id).await;
 
     match message_result {
@@ -38,12 +38,12 @@ pub async fn get_message<T: QueryMessageFn>(app_data: web::Data<AppState<T>>, pa
                 None => Ok(None)
             }
         },
-        Err(e) => Err(Box::new(e))
+        Err(e) => Err(e.into())
     }
 }
 
 #[allow(unused)]
-pub async fn get_messages<T: QueryMessagesFn>(app_data: web::Data<AppState<T>>, path: Path<MessageByFollowingQuery>) -> Result<impl Responder, Box<dyn Error>>  {
+pub async fn get_messages<T: QueryMessagesFn>(app_data: web::Data<AppState<T>>, path: Path<MessageByFollowingQuery>) -> Result<impl Responder, UserError>  {
     let page_size = match path.page_size {
         Some(ps) => ps,
         None => 10
@@ -63,7 +63,7 @@ pub async fn get_messages<T: QueryMessagesFn>(app_data: web::Data<AppState<T>>, 
 
             Ok(Json(msg_collection))
         },
-        Err(e) => Err(Box::new(e))
+        Err(e) => Err(e.into())
     }
 }
 
@@ -101,12 +101,13 @@ fn convert(message: &MessageWithFollowingAndBroadcastQueryResult) -> MessageResp
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use actix_web::web::Json;
+    use async_trait::async_trait;
+    use crate::{common::entities::messages::repo::InsertMessageFn, routes::messages::{message_route::create_message, model::MessagePostJson}, common_tests::actix_fixture::{get_app_data, get_fake_message_body}};
+    
 
-    mod test_mod_create_message_and_check_id {
-        use actix_web::web::Json;
-        use async_trait::async_trait;
-        use crate::{common::entities::messages::repo::InsertMessageFn, routes::messages::{message_route::create_message, model::MessagePostJson}, common_tests::actix_fixture::{get_app_data, get_fake_message_body}};
+    mod test_mod_create_message_and_check_id {        
+        use super::*;
 
         const ID: i64 = 22;
         struct TestRepo;
@@ -136,6 +137,100 @@ mod tests {
 
             assert!(!result.is_err());
             assert!(result.ok().unwrap().id == ID);
+        }
+    }
+
+    mod test_mod_create_message_failure_returns_correct_error {      
+        use crate::routes::errors::error_utils::UserError;
+        use super::*;
+
+        struct TestRepo;
+        
+        #[allow(unused)]
+        #[async_trait]
+        impl InsertMessageFn for TestRepo {            
+            async fn insert_message(
+                &self,
+                user_id: i64,
+                body: &str,
+                group_type: i32,
+                broadcasting_msg_id: Option<i64>
+            ) -> Result<i64, sqlx::Error> {
+                Err(sqlx::Error::PoolTimedOut)
+            }
+        }
+
+        #[tokio::test]
+        async fn test_create_message_failure_returns_correct_error () {
+            let repo = TestRepo;
+            let app_data = get_app_data(repo).await;
+
+            let result = create_message(app_data, Json(
+                MessagePostJson{ user_id: 0, body: get_fake_message_body(None), group_type: crate::routes::messages::model::GroupTypes::Circle, broadcasting_msg_id: None }
+            )).await;
+
+            assert!(result.is_err());
+            assert!(result.err().unwrap() == UserError::InternalError);
+        }
+    }
+
+    mod test_mod_get_message_failure_returns_correct_error {      
+        use actix_web::web::Path;
+        use crate::{routes::{errors::error_utils::UserError, messages::{message_route::get_message, model::MessageQuery}}, common::entities::messages::{repo::QueryMessageFn, model::MessageWithFollowingAndBroadcastQueryResult}};
+        use super::*;
+
+        struct TestRepo;
+        
+        #[allow(unused)]
+        #[async_trait]
+        impl QueryMessageFn for TestRepo {            
+            async fn query_message(&self, id: i64) -> Result<Option<MessageWithFollowingAndBroadcastQueryResult>, sqlx::Error> {
+                Err(sqlx::Error::ColumnNotFound("na".to_string()))
+            }
+        }
+
+        #[tokio::test]
+        async fn test_get_message_failure_returns_correct_error () {
+            let repo = TestRepo;
+            let app_data = get_app_data(repo).await;
+
+            let result = get_message(app_data, Path::from(MessageQuery{ id: 0 })).await;
+
+            assert!(result.is_err());
+            assert!(result.err().unwrap() == UserError::InternalError);
+        }
+    }
+
+    mod test_mod_get_messages_failure_returns_correct_error {      
+        use actix_web::web::Path;
+        use chrono::{DateTime, Utc};
+        use crate::{routes::{errors::error_utils::UserError, messages::{message_route::get_messages, model::{MessageQuery, MessageByFollowingQuery}}}, common::entities::messages::{repo::QueryMessagesFn, model::MessageWithFollowingAndBroadcastQueryResult}};
+        use super::*;
+
+        struct TestRepo;
+        
+        #[allow(unused)]
+        #[async_trait]
+        impl QueryMessagesFn for TestRepo {            
+            async fn query_messages(
+                &self, 
+                user_id: i64,
+                last_updated_at: DateTime<Utc>,
+                page_size: i16
+            ) -> Result<Vec<MessageWithFollowingAndBroadcastQueryResult>, sqlx::Error> {
+                Err(sqlx::Error::ColumnNotFound("na".to_string()))
+            }
+        }
+
+        #[tokio::test]
+        async fn test_get_messages_failure_returns_correct_error () {
+            let repo = TestRepo;
+            let app_data = get_app_data(repo).await;
+
+            let result = get_messages(app_data, Path::from(MessageByFollowingQuery { follower_id: 0, last_updated_at: Utc::now(), page_size: None })).await;
+
+            assert!(result.is_err());
+            assert!(result.err().unwrap() == UserError::InternalError);
         }
     }
 }
